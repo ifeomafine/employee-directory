903d9b24a7767fd1b3427c4e6663b02a147a10e3
ifeomafine <102186386+ifeomafine@users.noreply.github.com>
2022-05-31 07:25:28 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// alert('how are you?')
+
+const bar = document.querySelector('#searchBar')
+const icon = document.querySelector('#pic')
+const names = document.querySelectorAll('.names')
+const title = document.querySelectorAll('.title')
+const type = document.querySelectorAll('.type')
+
+bar.addEventListener('keyup' , (e) => {})
+// console.log(title)
