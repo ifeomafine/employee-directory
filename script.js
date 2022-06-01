c7ad2a8f27110f98be68e89422898a55b4d088fe
ifeomafine <102186386+ifeomafine@users.noreply.github.com>
2022-06-01 01:00:47 +0100

    Javascript Functionality added

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 // alert('how are you?')
 
-const bar = document.querySelector('#searchBar')
-const icon = document.querySelector('#pic')
-const names = document.querySelectorAll('.names')
-const title = document.querySelectorAll('.title')
-const type = document.querySelectorAll('.type')
+const bar = document.querySelector('#searchBar') /*same as searchbar */
+const card = document.querySelectorAll('.cardOne') /*same as employee */
+// console.log(card)
 
-bar.addEventListener('keyup' , (e) => {})
-// console.log(title)
+const displayEmployees = (values) => {
+    card.forEach(element => {
+      element.style.display = 'none'
+      const name = element.children[1].innerText.toUpperCase();
+      const title = element.children[2].innerText.toUpperCase();
+      const type = element.children[3].innerText.toUpperCase();
+
+      if (name.includes(values) || title.includes(values) || type.includes(values)) {
+        element.style.display = 'block'
+      }
+    } )
+}
+
+ bar.addEventListener('keyup' , (e) => {
+    displayEmployees(e.target.value.toUpperCase())
+}) ;
